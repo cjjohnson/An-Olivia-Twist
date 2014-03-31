10cb023b174554bd54e8e83abe1546e55f346383
DJ Heaney <chipsolderer@gmail.com>
2014-03-30 18:11:30 -0600

    Fixed blog issues

--- blog.js
+++ blog.js
@@ -1,3 +1,17 @@
+var reset_disqus = function(identifier) {
+    if(typeof DISQUS == 'undefined') {
+        setTimeout(function() { reset_disqus(identifier); }, 100);
+    } else {
+        DISQUS.reset({
+            reload: true,
+            config: function () {  
+                this.page.identifier = identifier;  
+                this.page.url = window.location.href;
+            }
+        });
+    }
+};
+
 $(function () {
     var USERNAME = "anoliviatwist";
 
@@ -67,25 +81,27 @@ $(function () {
 	    window.location.href = window.location.origin + window.location.pathname + "#!" + value.slug;
             disqus_title = $(value.title[0]).html();
 
-            DISQUS.reset({
-                reload: true,
-                config: function () {  
-                    this.page.identifier = $(value.title[0]).html();  
-                    this.page.url = window.location.href;
-                }
-            });
-
             $("#current_post").html("");
             $("#current_post").append($("<h3/>", { html: $(value.title[0]).clone().html() }));
             $("#current_post").append($(value.date[0]).clone());
             $("#current_post").append($(value.content[0]).clone());
+
+            reset_disqus(disqus_title);
         });
     });
 
-    if(window.location.hash) {
+    var update = function() {
         var id = window.location.hash.slice(1).replace('!', '');
         $("#" + id).click();
+    }
+
+    if(window.location.hash) {
+        update();
     } else {
         $("#post_list p").get(0).click();
+    }
+
+    window.onhashchange = function() {
+        update();
     }
 });
